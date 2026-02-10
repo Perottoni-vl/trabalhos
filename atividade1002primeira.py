0017af93476c55d4bcaf2453c8a170ad65c03104
Perottoni-vl <arthurperottonifrigeri@gmail.com>
2026-02-10 11:15:46 -0400

    Create atividade1002primeira.py
    push

---
+++ atividade1002primeira.py
@@ -0,0 +1,20 @@
+# algoritmo : calcula média
+* leitura das notas
+
+n1 = float(input(" Digite a primeira nota"))
+n2 = float(input(" Digite a segunda nota"))
+n3 = float(input(" Digite a terceira nota"))
+
+# cálculo da média 
+
+media = (n1 + n2 + n3/3)
+
+# verificação da situação do aluno
+ If media >=6
+  print ("Aprovado")
+ Else
+  print ("Reprovado")
+
+
+--professor quer q suba isso pro git depois
+# para subir pro git    
