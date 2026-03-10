326b39e5cdd624c0c2cd82ec71028acc6284e8ec
Perottoni-vl <arthurperottonifrigeri@gmail.com>
2026-03-10 15:30:15 -0400

    Update Atividade_Algorítmos_AllCodes.py

--- atividade_algorítmos_março/Atividade_Algorítmos_AllCodes.py
+++ atividade_algorítmos_março/Atividade_Algorítmos_AllCodes.py
@@ -1,7 +1,10 @@
 
 ## folha: algo_exer_selecao
 
-# 1
+#exercício_1
+import math
+
+
 numero = int(input("Digite um número inteiro: "))
 
 if numero == 0:
@@ -10,7 +13,7 @@ else:
     print("Diferente de Zero")
 
 
-# 2
+#exercício_2
 numero = int(input("Digite um número entre 1 e 9: "))
 if numero <= 9 and numero >= 1:
     print("Valor na faixa.")
@@ -18,7 +21,8 @@ else:
     print("Valor fora da faixa.")
 
 
-# 3 triângulo retângulo
+#exercício_3
+# triângulo retângulo
 ladoA = int(input("Digite o primeiro lado: "))
 ladoB = int(input("Digite o segundo lado: "))
 ladoC = int(input("Digite o terceiro lado: "))
@@ -40,7 +44,8 @@ if maior**2 == cat1**2 + cat2**2:
 else:
     print("O triângulo não é retângulo.")
 
-# 4 triângulo equilátero
+#exercício_4
+# triângulo equilátero
 ladoA = int(input("Digite o primeiro lado: "))
 ladoB = int(input("Digite o segundo lado: "))   
 ladoC = int(input("Digite o terceiro lado: "))
@@ -50,13 +55,72 @@ if ladoA == ladoB == ladoC:
 else:
     print("O triângulo não é equilátero.")
 
-# 5 triângulo isósceles
+
+#exercício_5
+# triângulo isósceles (pode conter os três lados de valores iguais)
 ladoA = int(input("Digite o primeiro lado: "))
 ladoB = int(input("Digite o segundo lado: "))
 ladoC = int(input("Digite o terceiro lado: "))
 
-if ladoA == ladoB or ladoA == ladoC or ladoB == ladoC:
+if ladoA == ladoB:
     print("O triângulo é isósceles.")
-    else
+elif ladoA == ladoC:
+    print("O triângulo é isósceles.")
+elif ladoB == ladoC:
+    print("O triângulo é isósceles.")
+elif ladoA == ladoB == ladoC:
     print("O triângulo não é isósceles.")
+else:
+    print("O triângulo não é isósceles.")
+
+
+#exercício_6
+# triângulo escaleno
+ladoA = int(input("Digite o primeiro lado: "))
+ladoB = int(input("Digite o segundo lado: "))
+ladoC = int(input("Digite o terceiro lado: "))
+
+if ladoA != ladoB and ladoA != ladoC and ladoB != ladoC:
+    print("O triângulo é escaleno.")
+else:
+    print("O triângulo não é escaleno.")
+
+
+#exercício_7
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+
+maior = max(valor1, valor2)
+menor = min(valor1, valor2)
+
+maior/menor = valor3
+
+print("O resultado da divisão do maior pelo menor é:", valor3)
+
+#exercício_8 (esse eu não consegui fazer, tive que usar ferramentas externas para resolver. 
+# Porém, aprendi mais sobre a linguagem Python)
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+valor3 = int(input("Digite o terceiro valor: "))
+
+delta = valor2**2 - 4*valor1*valor3
+
+if delta < 0:
+    print("Não Existe Valores Reais")
+else:
+    x1 = (-valor2 + math.sqrt(delta)) / (2*valor1)
+    x2 = (-valor2 - math.sqrt(delta)) / (2*valor1)
+
+    print("Valor de x1:", x1)
+    print("Valor de x2:", x2)
+
+
+
+
+
+
+
+
+
 
