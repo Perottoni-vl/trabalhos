4543fdea316a4a3b35c9c352d0ea8eb89e8bdf95
Perottoni-vl <arthurperottonifrigeri@gmail.com>
2026-03-31 09:21:43 -0400

    configuração

--- Arthur_Coisas/conexao.py
+++ Arthur_Coisas/conexao.py
@@ -10,7 +10,7 @@ def conectar_banco():
         # Tente escrever o nome do banco sem aspas extras primeiro
         # Se o erro persistir, o problema pode estar no 'host' ou na 'senha'
         conexao = psycopg2.connect(
-            host="127.0.0.1", # Usar o IP local evita erros de decodificação de nome
+            host="127.0.0.1", 
             database="Teste do Arthur",
             user="postgres",
             password="12345",
